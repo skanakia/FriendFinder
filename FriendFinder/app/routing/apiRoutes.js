2806ad7878e6f9c7cab7db73a93e221f391a673a
skanakia <skanakia1@gmail.com>
2018-06-10 21:56:26 -0400

    added modal

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -12,29 +12,31 @@ module.exports = function(app){
 
     app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
-        var newScore = newFriend.answers;
+        var newResponses = newFriend.answers;
         var newTotal = 0;
         var bestScore = 10000000;
         var bestName = "";
         var bestPhoto = "";
+        var index = -1;
 
         for(var i = 0; i < friendList.length; i++){
             NewTotal = 0;
-
-            for(var j = 0; j < newScore.length; j++){
-                var diff = Math.abs(newScore[j] - friendList[i].answers[j]);
+            for(var j = 0; j < newFriend.answers.length; j++){
+                var diff = Math.abs(newFriend.answers[j] - friendList[i].answers[j]);
                 newTotal += diff;
             }
             if(newTotal < bestScore){
                 bestScore = NewTotal;
                 bestName = friendList[i].name;
                 bestPhoto = friendList[i].photo;
+                index = i;
             }
         }
+        
         console.log('Best Match: ' + bestName);
         console.log("link to best photo: " + bestPhoto)
         friendList.push(newFriend);
-        
+        res.json(friendList[index]);
     });
 };
 
